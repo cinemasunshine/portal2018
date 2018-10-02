a51648799fc5f055b454213207708cbd3e59c2de
MP奥井 <okui@motionpicture.jp>
2018-10-02 13:41:41 +0900

    SNSのURLを取得するTwig拡張機能を追加

--- src/Twig/Extension/CommonExtension.php
+++ src/Twig/Extension/CommonExtension.php
@@ -22,6 +22,41 @@ class CommonExtension extends \Twig_Extension
     }
     
     /**
+     * get functions
+     *
+     * @return array
+     */
+    public function getFunctions()
+    {
+        return [
+            new \Twig_Function('facebook', [$this, 'getFacebookUrl']),
+            new \Twig_Function('twitter', [$this, 'getTwitterUrl']),
+        ];
+    }
+    
+    /**
+     * retrun facebook URL
+     *
+     * @param string $name
+     * @return string
+     */
+    public function getFacebookUrl(string $name)
+    {
+        return sprintf('https://www.facebook.com/%s', $name);
+    }
+    
+    /**
+     * return twitter URL
+     *
+     * @param string $name
+     * @return string
+     */
+    public function getTwitterUrl(string $name)
+    {
+        return sprintf('https://twitter.com/%s', $name);
+    }
+    
+    /**
      * get filters
      *
      * @return array
