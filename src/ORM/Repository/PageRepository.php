55a7b7ebaa10a8b6289c8e06ec6af3df87f7e564
MP奥井 <okui@motionpicture.jp>
2018-10-05 14:51:52 +0900

    エンティティPageにrepositoryClassを設定

---
+++ src/ORM/Repository/PageRepository.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * PageRepository.php
+ * 
+ * @author Atsushi Okui <okui@motionpicture.jp>
+ */
+
+namespace Cinemasunshine\Portal\ORM\Repository;
+
+use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\QueryBuilder;
+
+use Cinemasunshine\Portal\ORM\Entity\Page;
+
+/**
+ * Page repository class
+ */
+class PageRepository extends EntityRepository
+{
+    /**
+     * return active query
+     *
+     * @return QueryBuilder
+     */
+    protected function getActiveQuery()
+    {
+        $qb = $this->createQueryBuilder('p');
+        $qb->where('p.isDeleted = false');
+        
+        return $qb;
+    }
+    
+    /**
+     * find one by id
+     *
+     * @param int $id
+     * @return Page|null
+     */
+    public function findOneById(int $id)
+    {
+        $qb = $this->getActiveQuery();
+        $qb
+            ->andWhere('p.id = :id')
+            ->setParameter('id', $id);
+            
+        return $qb->getQuery()->getOneOrNullResult();
+    }
+}
