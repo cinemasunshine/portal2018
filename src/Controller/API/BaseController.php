94a4bb5645210de20199a8964c072390a3ac3551
MP奥井 <okui@motionpicture.jp>
2018-10-07 20:01:28 +0900

    APIのベースを実装

---
+++ src/Controller/API/BaseController.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * BaseController.php
+ * 
+ * @author Atsushi Okui <okui@motionpicture.jp>
+ */
+
+namespace Cinemasunshine\Portal\Controller\API;
+
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ResponseInterface as Response;
+
+use Cinemasunshine\Portal\Controller\AbstractController;
+use Cinemasunshine\Portal\Responder;
+use Cinemasunshine\Portal\Responder\API as ApiResponder;
+
+/**
+ * Base controller
+ */
+abstract class BaseController extends AbstractController
+{
+    /**
+     * pre execute
+     * 
+     * @param \Slim\Http\Request  $request
+     * @param \Slim\Http\Response $response
+     * @return void
+     */
+    protected function preExecute($request, $response) : void
+    {
+    }
+    
+    /**
+     * post execute
+     *
+     * @param \Slim\Http\Request  $request
+     * @param \Slim\Http\Response $response
+     * @return void
+     */
+    protected function postExecute($request, $response) : void
+    {
+    }
+    
+    /**
+     * get responder
+     *
+     * @return Responder\AbstractResponder
+     */
+    protected function getResponder() : Responder\AbstractResponder
+    {
+        $path = explode('\\', get_class($this));
+        $container = str_replace('Controller', '', array_pop($path));
+        $responder = ApiResponder::class . '\\' . $container . 'Responder';
+        
+        return new $responder($this->view);
+    }
+}
---
+++ src/Responder/API/BaseResponder.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * BaseResponder.php
+ * 
+ * @author Atsushi Okui <okui@motionpicture.jp>
+ */
+
+namespace Cinemasunshine\Portal\Responder\API;
+
+use Cinemasunshine\Portal\Responder\AbstractResponder;
+
+/**
+ * Base responder
+ */
+abstract class BaseResponder extends AbstractResponder
+{
+    /**
+     * contsruct
+     */
+    public function __construct()
+    {
+    }
+}
