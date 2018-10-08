e3d5743e30c737540540b0c3872facb5b6054d8b
MP奥井 <okui@motionpicture.jp>
2018-10-08 13:12:46 +0900

    フロントエンドAPIを使用するためのjavascriptを追加

---
+++ public/js/api.js
@@ -0,0 +1,38 @@
+/**
+ * API
+ */
+var api;
+
+$(function(){
+    api = {};
+    api.schedule = {};
+    
+    /**
+     * fetch schedule list
+     * 
+     * @param {String} theater
+     * @returns {jqXHR}
+     */
+    api.schedule.list = function(theater) {
+        return $.ajax({
+            url: '/api/schedule/' + theater,
+            cache: false,
+            dataType: 'json'
+        });
+    };
+    
+    /**
+     * fetch schedule for date
+     * 
+     * @param {String} theater
+     * @param {String} date    YYYY-MM-DD
+     * @returns {jqXHR}
+     */
+    api.schedule.date = function(theater, date) {
+        return $.ajax({
+            url: '/api/schedule/' + theater + '/' + date,
+            cache: false,
+            dataType: 'json'
+        });
+    };
+});
