00d51e0ecb7f166f7ed6d4c22d53182a026c9cd8
MP奥井 <okui@motionpicture.jp>
2018-10-10 13:44:25 +0900

    現行ポータルに合わせる形でURLを変更
    新しいページも同じような構成にする

--- config/routes.php
+++ config/routes.php
@@ -19,41 +19,41 @@ use Cinemasunshine\Portal\Controller\API\ScheduleController as ScheduleApiContro
 
 $app->get('/', IndexController::class . ':index')->setName('homepage');
 
-$app->get('/company-profile', AboutController::class . ':company')->setName('company');
-$app->get('/mail-magazine', AboutController::class . ':mailMagazine')->setName('mail_magazine');
+$app->get('/company', AboutController::class . ':company')->setName('company');
+$app->get('/magazine', AboutController::class . ':mailMagazine')->setName('mail_magazine');
 $app->get('/mvtk', AboutController::class . ':mvtk')->setName('mvtk');
 $app->get('/app', AboutController::class . ':officialApp')->setName('official_app');
-$app->get('/online-ticket', AboutController::class . ':onlineTicket')->setName('online_ticket');
+$app->get('/online', AboutController::class . ':onlineTicket')->setName('online_ticket');
 $app->get('/privacy', AboutController::class . ':privacy')->setName('privacy');
 $app->get('/question', AboutController::class . ':question')->setName('question');
 $app->get('/sitemap', AboutController::class . ':sitemap')->setName('sitemap');
 $app->get('/special_ticket', AboutController::class . ':specialTicket')->setName('special_ticket');
-$app->get('/specific-quotient', AboutController::class . ':specificQuotient')->setName('specific_quotient');
-$app->get('/terms-of-service', AboutController::class . ':termsOfService')->setName('terms_of_service');
+$app->get('/law', AboutController::class . ':specificQuotient')->setName('specific_quotient');
+$app->get('/sitepolicy', AboutController::class . ':termsOfService')->setName('terms_of_service');
 
 $app->group('/news', function () {
-    $this->get('/list', NewsController::class . ':list')->setName('news_list');
+    $this->get('', NewsController::class . ':list')->setName('news_list');
     $this->get('/{id}', NewsController::class . ':show')->setName('news_show');
 });
 
 $app->group('/movie', function() {
-    $this->get('/list', ScheduleController::class . ':list')->setName('schedule_list');
+    $this->get('', ScheduleController::class . ':list')->setName('schedule_list');
     $this->get('/{schedule}', ScheduleController::class . ':show')->setName('schedule_show');
 });
 
-$app->get('/theater-list', TheaterListController::class . ':index')->setName('theater_list');
-$app->get('/theater-sns-list', TheaterListController::class . ':sns')->setName('theater_sns');
+$app->get('/theater', TheaterListController::class . ':index')->setName('theater_list');
+$app->get('/theater-sns', TheaterListController::class . ':sns')->setName('theater_sns');
 
 $app->group('/theater/{name}', function () {
     $this->get('', TheaterController::class . ':index')->setName('theater');
     $this->get('/access', TheaterController::class . ':access')->setName('theater_access');
     $this->get('/admission', TheaterController::class . ':admission')->setName('theater_admission');
-    $this->get('/advance-tickets', TheaterController::class . ':advanceTicket')->setName('theater_advance_ticket');
+    $this->get('/advance_ticket', TheaterController::class . ':advanceTicket')->setName('theater_advance_ticket');
     $this->get('/concession', TheaterController::class . ':concession')->setName('theater_concession');
-    $this->get('/facility', TheaterController::class . ':floorGuide')->setName('theater_floor_guide');
+    $this->get('/floor_guide', TheaterController::class . ':floorGuide')->setName('theater_floor_guide');
     
     $this->group('/news', function () {
-        $this->get('/list', TheaterController::class . ':newsList')->setName('theater_news_list');
+        $this->get('', TheaterController::class . ':newsList')->setName('theater_news_list');
         $this->get('/{id}', TheaterController::class . ':newsShow')->setName('theater_news_show');
     });
 });
@@ -63,9 +63,9 @@ $app->get('/trailer', TrailerController::class . ':show')->setName('trailer');
 $app->group('/imax', function() {
     $this->get('', ImaxController::class . ':index')->setName('imax');
     $this->get('/about', ImaxController::class . ':about')->setName('imax_about');
-    $this->get('/movie/list', ImaxController::class . ':scheduleList')->setName('imax_schedule_list');
+    $this->get('/movie', ImaxController::class . ':scheduleList')->setName('imax_schedule_list');
     $this->get('/movie/{schedule}', ImaxController::class . ':scheduleShow')->setName('imax_schedule_show');
-    $this->get('/news/list', ImaxController::class . ':newsList')->setName('imax_news_list');
+    $this->get('/news', ImaxController::class . ':newsList')->setName('imax_news_list');
     $this->get('/news/{id}', ImaxController::class . ':newsShow')->setName('imax_news_show');
     $this->get('/theater', ImaxController::class . ':theater')->setName('imax_theater');
 });
@@ -73,9 +73,9 @@ $app->group('/imax', function() {
 $app->group('/4dx', function() {
     $this->get('', FourdxController::class . ':index')->setName('4dx');
     $this->get('/about', FourdxController::class . ':about')->setName('4dx_about');
-    $this->get('/movie/list', FourdxController::class . ':scheduleList')->setName('4dx_schedule_list');
+    $this->get('/movie', FourdxController::class . ':scheduleList')->setName('4dx_schedule_list');
     $this->get('/movie/{schedule}', FourdxController::class . ':scheduleShow')->setName('4dx_schedule_show');
-    $this->get('/news/list', FourdxController::class . ':newsList')->setName('4dx_news_list');
+    $this->get('/news', FourdxController::class . ':newsList')->setName('4dx_news_list');
     $this->get('/news/{id}', FourdxController::class . ':newsShow')->setName('4dx_news_show');
     $this->get('/theater', FourdxController::class . ':theater')->setName('4dx_theater');
 });
