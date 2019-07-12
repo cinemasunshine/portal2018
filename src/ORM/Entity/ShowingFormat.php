19cd4a970582587498f53a359af19e13079400d1
Atsushi Okui <okui@motionpicture.jp>
2019-07-12 15:32:43 +0900

    エンティティShowingFormatにsoundを追加 SASAKI-449
    音響を設定するカラム

--- src/ORM/Entity/ShowingFormat.php
+++ src/ORM/Entity/ShowingFormat.php
@@ -33,6 +33,12 @@ class ShowingFormat extends AbstractEntity
     const SYSTEM_4DX_WITH_SCREENX = 11; // SASAKI-428
     const SYSTEM_NONE             = 99;
     
+    const SOUND_BESTIA        = 1;
+    const SOUND_DTSX          = 2;
+    const SOUND_DOLBY_ATMOS   = 3;
+    const SOUND_GDC_IMMERSIVE = 4;
+    const SOUND_NONE          = 99;
+    
     const VOICE_SUBTITLE = 1;
     const VOICE_DUB = 2;
     const VOICE_NONE = 3;
@@ -63,6 +69,14 @@ class ShowingFormat extends AbstractEntity
      * @ORM\Column(type="smallint", nullable=false, options={"unsigned"=true})
      */
     protected $system;
+    
+    /**
+     * sound
+     *
+     * @var int
+     * @ORM\Column(type="smallint", nullable=false, options={"unsigned"=true})
+     */
+    protected $sound;
     
     /**
      * voice
@@ -132,6 +146,28 @@ class ShowingFormat extends AbstractEntity
      * @throws \LogicException
      */
     public function setSystem(int $system)
+    {
+        throw new \LogicException('Not allowed.');
+    }
+    
+    /**
+     * get sound
+     *
+     * @return int
+     */
+    public function getSound()
+    {
+        return $this->sound;
+    }
+    
+    /**
+     * set sound
+     *
+     * @param int $sound
+     * @return void
+     * @throws \LogicException
+     */
+    public function setSound(int $sound)
     {
         throw new \LogicException('Not allowed.');
     }
