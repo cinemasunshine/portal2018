b928cc87883d2c4189f2921d09d59df5252c7bea
Atsushi Okui <okui@motionpicture.jp>
2019-10-30 22:17:51 +0900

    認可トークン取得APIのエンドポイントを追加 SASAKI-479

---
+++ src/Controller/API/AuthorizationController.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * AuthorizationController.php
+ *
+ * @author Atsushi Okui <okui@motionpicture.jp>
+ */
+
+namespace Cinemasunshine\Portal\Controller\API;
+
+/**
+ * Authorization controller
+ */
+class AuthorizationController extends BaseController
+{
+    /**
+     * token action
+     *
+     * @param \Slim\Http\Request  $request
+     * @param \Slim\Http\Response $response
+     * @param array               $args
+     * @return string|void
+     */
+    public function executeToken($request, $response, $args)
+    {
+    }
+}
---
+++ src/Responder/API/AuthorizationResponder.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * AuthorizationResponder.php
+ *
+ * @author Atsushi Okui <okui@motionpicture.jp>
+ */
+
+namespace Cinemasunshine\Portal\Responder\API;
+
+use Slim\Collection;
+use Slim\Http\Response;
+
+/**
+ * Authorization responder
+ */
+class AuthorizationResponder extends BaseResponder
+{
+    /**
+     * token
+     *
+     * @param Response   $response
+     * @param Collection $data
+     * @return Response
+     */
+    public function token(Response $response, Collection $data)
+    {
+        return $response->withJson($data->all());
+    }
+}
