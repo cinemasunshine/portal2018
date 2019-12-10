adaded800cc1ac99372cb254001432c5e3e788f2
Atsushi Okui <okui@motionpicture.jp>
2019-12-10 21:09:26 +0900

    エンティティを更新 SASAKI-527

---
+++ src/ORM/Entity/OyakoCinemaTitle.php
@@ -0,0 +1,134 @@
+<?php
+/**
+ * OyakoCinemaTitle.php
+ *
+ * @author Atsushi Okui <okui@motionpicture.jp>
+ */
+
+namespace Cinemasunshine\Portal\ORM\Entity;
+
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * OyakoCinemaTitle entity class
+ *
+ * @ORM\Entity
+ * @ORM\Table(name="oyako_cinema_title", options={"collate"="utf8mb4_general_ci"})
+ * @ORM\HasLifecycleCallbacks
+ */
+class OyakoCinemaTitle extends AbstractEntity
+{
+    use SavedUserTrait;
+    use SoftDeleteTrait;
+    use TimestampableTrait;
+
+    /**
+     * id
+     *
+     * @var int
+     * @ORM\Id
+     * @ORM\Column(type="integer", options={"unsigned"=true})
+     * @ORM\GeneratedValue
+     */
+    protected $id;
+
+    /**
+     * title
+     *
+     * @var Title
+     * @ORM\ManyToOne(targetEntity="Title")
+     * @ORM\JoinColumn(name="title_id", referencedColumnName="id", nullable=false, onDelete="RESTRICT")
+     */
+    protected $title;
+
+    /**
+     * title_url
+     *
+     * @var string
+     * @ORM\Column(type="string", name="title_url")
+     */
+    protected $titleUrl;
+
+    /**
+     * oyako_cinema_schedules
+     *
+     * @var Collection
+     * @ORM\OneToMany(targetEntity="OyakoCinemaSchedule", mappedBy="oyakoCinemaTitle", orphanRemoval=true)
+     */
+    protected $oyakoCinemaSchedules;
+
+    /**
+     * construct
+     *
+     * @throws \LogicException
+     */
+    public function __construct()
+    {
+        throw new \LogicException('Not allowed.');
+    }
+
+    /**
+     * get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * get title
+     *
+     * @return Title
+     */
+    public function getTitle(): Title
+    {
+        return $this->title;
+    }
+
+    /**
+     * set title
+     *
+     * @param Title $title
+     * @return void
+     * @throws \LogicException
+     */
+    public function setTitle(Title $title)
+    {
+        throw new \LogicException('Not allowed.');
+    }
+
+    /**
+     * get title_url
+     *
+     * @return string
+     */
+    public function getTitleUrl(): string
+    {
+        return $this->titleUrl;
+    }
+
+    /**
+     * set title_url
+     *
+     * @param string $titleUrl
+     * @return void
+     * @throws \LogicException
+     */
+    public function setTitleUrl(string $titleUrl)
+    {
+        throw new \LogicException('Not allowed.');
+    }
+
+    /**
+     * get oyako_cinema_schedules
+     *
+     * @return Collection
+     */
+    public function getOyakoCinemaSchedules(): Collection
+    {
+        return $this->oyakoCinemaSchedules;
+    }
+}
