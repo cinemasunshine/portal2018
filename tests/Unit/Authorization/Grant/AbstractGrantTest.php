cd3759d5832948e3776fa98d3d1dc8a7b6a79072
Atsushi Okui <okui@motionpicture.jp>
2020-06-17 13:50:49 +0900

    Add Authorization tests

---
+++ tests/Unit/Authorization/Grant/AbstractGrantTest.php
@@ -0,0 +1,89 @@
+<?php
+
+/**
+ * AbstractGrantTest.php
+ *
+ * @author Atsushi Okui <okui@motionpicture.jp>
+ */
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Authorization\Grant;
+
+use Cinemasunshine\Portal\Authorization\Grant\AbstractGrant;
+use GuzzleHttp\Client as HttpClient;
+use Mockery;
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * AbstractGrant test
+ */
+final class AbstractGrantTest extends TestCase
+{
+    use MockeryPHPUnitIntegration;
+
+    /**
+     * Create target mock
+     *
+     * @return \Mockery\MockInterface|\Mockery\LegacyMockInterface|AbstractGrant
+     */
+    protected function createTargetMock()
+    {
+        return Mockery::mock(AbstractGrant::class);
+    }
+
+    /**
+     * Create target reflection
+     *
+     * @return \ReflectionClass
+     */
+    protected function createTargetReflection()
+    {
+        return new \ReflectionClass(AbstractGrant::class);
+    }
+
+    /**
+     * test createHttpClient
+     *
+     * @test
+     * @return void
+     */
+    public function testCreateHttpClient()
+    {
+        $baseUri = 'https://example.com';
+        $targetMock = $this->createTargetMock();
+        $targetRef = $this->createTargetReflection();
+
+        $targetMethodRef = $targetRef->getMethod('createHttpClient');
+        $targetMethodRef->setAccessible(true);
+
+        $result = $targetMethodRef->invoke($targetMock, $baseUri);
+
+        $this->assertInstanceOf(HttpClient::class, $result);
+        $this->assertEquals($baseUri, $result->getConfig('base_uri'));
+    }
+
+    /**
+     * test getRequestHeaders
+     *
+     * @test
+     * @return void
+     */
+    public function testGetRequestHeaders()
+    {
+        $clientId = 'client_id';
+        $clientSecret = 'client_secret';
+
+        $targetMock = $this->createTargetMock();
+        $targetRef = $this->createTargetReflection();
+
+        $targetMethodRef = $targetRef->getMethod('getRequestHeaders');
+        $targetMethodRef->setAccessible(true);
+
+        $result = $targetMethodRef->invoke($targetMock, $clientId, $clientSecret);
+        $this->assertIsArray($result);
+        $this->assertArrayHasKey('Authorization', $result);
+        $this->assertArrayHasKey('Content-Type', $result);
+    }
+}
