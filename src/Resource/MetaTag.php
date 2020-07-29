90f3048785f37862c23a0943e2a6345e2024876d
Atsushi Okui <okui@motionpicture.jp>
2020-07-29 21:04:01 +0900

    metaタグの内容をjsonで管理する SASAKI-578

---
+++ src/Resource/MetaTag.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * MetaTag.php
+ *
+ * @author Atsushi Okui <okui@motionpicture.jp>
+ */
+
+declare(strict_types=1);
+
+namespace Cinemasunshine\Portal\Resource;
+
+class MetaTag
+{
+    /** @var string */
+    protected $title;
+
+    /** @var string */
+    protected $description;
+
+    /** @var string */
+    protected $keywords;
+
+    /**
+     * @param string $title
+     * @param string $description
+     * @param string $keywords
+     */
+    public function __construct(string $title, string $description, string $keywords)
+    {
+        $this->title = $title;
+        $this->description = $description;
+        $this->keywords = $keywords;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    /**
+     * @return string
+     */
+    public function getKeywords(): string
+    {
+        return $this->keywords;
+    }
+}
