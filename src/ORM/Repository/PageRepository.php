8caec3a0b05f9e11bcdd61a8e24236d4ba9b6ebf
Atsushi Okui <okui@motionpicture.jp>
2021-02-05 16:40:09 +0900

    Updated cinemasunshine/portal2018-orm to v1.3

--- src/ORM/Repository/PageRepository.php
+++ src/ORM/Repository/PageRepository.php
@@ -1,40 +1,30 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\ORM\Repository;
 
 use App\ORM\Entity\Page;
-use Doctrine\ORM\EntityRepository;
-use Doctrine\ORM\QueryBuilder;
+use Cinemasunshine\ORM\Repositories\PageRepository as BaseRepository;
 
 /**
- * Page repository class
+ * @extends BaseRepository<Page>
  */
-class PageRepository extends EntityRepository
+class PageRepository extends BaseRepository
 {
     /**
-     * return active query
-     *
-     * @return QueryBuilder
-     */
-    protected function getActiveQuery()
-    {
-        $qb = $this->createQueryBuilder('p');
-        $qb->where('p.isDeleted = false');
-
-        return $qb;
-    }
-
-    /**
-     * find one by id
-     *
      * @param int $id
      * @return Page|null
      */
     public function findOneById(int $id)
     {
-        $qb = $this->getActiveQuery();
+        $alias = 'p';
+        $qb    = $this->createQueryBuilder($alias);
+
+        $this->addActiveQuery($qb, $alias);
+
         $qb
-            ->andWhere('p.id = :id')
+            ->andWhere($alias . '.id = :id')
             ->setParameter('id', $id);
 
         return $qb->getQuery()->getOneOrNullResult();
