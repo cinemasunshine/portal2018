cc5cc6e2e01ceba250bc2b5ac170892db76df488
Atsushi Okui <okui@motionpicture.jp>
2021-06-14 19:20:14 +0900

    Updated cinemasunshine/portal2018-orm to dev-feature/v2

---
+++ src/ORM/Entity/TitleRankingRank.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\ORM\Entity;
+
+use Cinemasunshine\ORM\Entities\Title as BaseTitle;
+use Cinemasunshine\ORM\Entities\TitleRanking as BaseTitleRanking;
+use Cinemasunshine\ORM\Entities\TitleRankingRank as BaseTitleRankingRank;
+use Doctrine\ORM\Mapping as ORM;
+use LogicException;
+
+/**
+ * TitleRankingRank entity class
+ *
+ * @ORM\Entity
+ * @ORM\Table(name="title_ranking_rank", options={"collate"="utf8mb4_general_ci"})
+ */
+class TitleRankingRank extends BaseTitleRankingRank
+{
+    /**
+     * {@inheritDoc}
+     *
+     * @throws LogicException
+     */
+    public function __construct()
+    {
+        throw new LogicException('Not allowed.');
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @throws LogicException
+     */
+    public function setRanking(BaseTitleRanking $ranking): void
+    {
+        throw new LogicException('Not allowed.');
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @throws LogicException
+     */
+    public function setTitle(?BaseTitle $title): void
+    {
+        throw new LogicException('Not allowed.');
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @throws LogicException
+     */
+    public function setRank(int $rank): void
+    {
+        throw new LogicException('Not allowed.');
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @throws LogicException
+     */
+    public function setDetailUrl(?string $detailUrl): void
+    {
+        throw new LogicException('Not allowed.');
+    }
+}
