cea9099ba37e2a49620b5917bdc479c61328156a
Atsushi Okui <okui@motionpicture.jp>
2023-04-26 18:04:28 +0900

    feat!: Client Credentials Tokenについて再設計
    BREAKING CHANGE: setterを廃止して不変にする

--- src/Authorization/Token/ClientCredentialsToken.php
+++ src/Authorization/Token/ClientCredentialsToken.php
@@ -4,15 +4,10 @@ declare(strict_types=1);
 
 namespace App\Authorization\Token;
 
-/**
- * Client Credentials Token class
- */
 class ClientCredentialsToken extends AbstractToken
 {
     protected string $accessToken;
-
     protected string $tokenType;
-
     protected int $expiresIn;
 
     /**
@@ -21,9 +16,10 @@ class ClientCredentialsToken extends AbstractToken
     public static function create(array $data): self
     {
         $token = new self();
-        $token->setAccessToken($data['access_token']);
-        $token->setTokenType($data['token_type']);
-        $token->setExpiresIn((int) $data['expires_in']);
+
+        $token->accessToken = $data['access_token'];
+        $token->tokenType   = $data['token_type'];
+        $token->expiresIn   = (int) $data['expires_in'];
 
         return $token;
     }
@@ -37,28 +33,13 @@ class ClientCredentialsToken extends AbstractToken
         return $this->accessToken;
     }
 
-    protected function setAccessToken(string $accessToken): void
-    {
-        $this->accessToken = $accessToken;
-    }
-
     public function getTokenType(): string
     {
         return $this->tokenType;
     }
 
-    protected function setTokenType(string $tokenType): void
-    {
-        $this->tokenType = $tokenType;
-    }
-
     public function getExpiresIn(): int
     {
         return $this->expiresIn;
-    }
-
-    public function setExpiresIn(int $expiresIn): void
-    {
-        $this->expiresIn = $expiresIn;
     }
 }
